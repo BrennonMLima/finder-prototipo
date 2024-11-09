68e4a0b0b7c5231a11638fdd4bfe04a7706a83af
Brennon Lima <brennonmlima@gmail.com>
2024-11-09 20:38:13 -0300

    removendo expiracao token

--- finder-backend/finder-backend/src/security/security.ts
+++ finder-backend/finder-backend/src/security/security.ts
@@ -5,44 +5,47 @@ import { UserDTO } from "../dto/user.dto";
 import { InternalException, NotFoundException } from "../exceptions/exceptions";
 
 export class SecurityClass {
-    static async encryptUserPassword(
-        password: string
-    ): Promise<string | undefined> {
-        return bcrypt.hash(password, 10);
+  static async encryptUserPassword(
+    password: string
+  ): Promise<string | undefined> {
+    return bcrypt.hash(password, 10);
+  }
+
+  static async authenticateUser(
+    email: string,
+    password: string
+  ): Promise<string> {
+    try {
+      const user = await UserService.getUserByEmail(email);
+      if (!user)
+        throw new NotFoundException(
+          "Não encontrado usuário com o seguinte email: ${ email }"
+        );
+
+      const userData = new UserDTO(
+        user.name,
+        user.email,
+        user.createdAt,
+        user.id
+      );
+      const isCorrectPassword = await this.verifyPassword(
+        password,
+        user.password
+      );
+
+      if (isCorrectPassword) {
+        return jwt.sign(userData.toJson(), "SECRET");
+      }
+    } catch (error) {
+      if (error instanceof NotFoundException) throw error;
+      throw new InternalException("Erro ao consultar a tabela de usuários");
     }
+  }
 
-    static async authenticateUser(
-        email: string,
-        password: string
-    ): Promise<string> {
-        try {
-            const user = await UserService.getUserByEmail(email);
-            if (!user)
-                throw new NotFoundException(
-                    'Não encontrado usuário com o seguinte email: ${ email }'
-                );
-
-            const userData = new UserDTO(
-                user.name,
-                user.email,
-                user.createdAt,
-                user.id
-            );
-            const isCorrectPassword = await this.verifyPassword(password, user.password)
-
-            if (isCorrectPassword) {
-                return jwt.sign(userData.toJson(), "SECRET", { expiresIn: "3h" });
-            }
-        } catch (error) {
-            if (error instanceof NotFoundException) throw error;
-            throw new InternalException("Erro ao consultar a tabela de usuários");
-        }
-    }
-
-    static async verifyPassword(
-        password: string,
-        userPassword: string
-    ): Promise<boolean> {
-        return await bcrypt.compare(password, userPassword);
-    }
+  static async verifyPassword(
+    password: string,
+    userPassword: string
+  ): Promise<boolean> {
+    return await bcrypt.compare(password, userPassword);
+  }
 }
